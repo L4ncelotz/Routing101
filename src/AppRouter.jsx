d09f8d3b84a7305d959207a06985b7c0ba5b0ec7
L4ncelotz <laycxz123@gmail.com>
2024-10-11 23:13:52 +0700

    fix-productdetails

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
+import ProductDetail from './pages/Productdetail';
 
 function AppRouter() {
   return (
